563dbdaed907235117a48bd6268a67ec5b5212f9
VButo <vbutorac@tvz.hr>
2025-05-07 13:58:04 +0200

    Add @vercel/analytics dependency and inject it in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
 import { createApp } from 'vue'
+import { inject } from '@vercel/analytics'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import socket from './socket'
 
+inject();
 const app = createApp(App)
 
 app.config.globalProperties.$socket = socket
